bfbd0d72bf1395c45d211598805deb99a7bc0c0d
Your Name <you@example.com>
2020-07-13 13:49:38 +0200

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> hello</h1>
+      <div class="container-fluid mt-3">
+         <h2>Ajouter une personne</h2>
+ 
+         <form>
+            
+            <div class="form-group">
+               <label for="myEmail">Email</label>
+               <input type="email" id = "myEmail" class="form-control" placeholder="Email"/>
+               <label for="myname">PRENOM</label>
+               <input type="nom" id="myname" class="form-control" placeholder="prenom"/>
+               <label for="last name">NOM</label>
+               <input type="nom" id="my last name" class="form-control" placeholder="nom"/>
+               <input class="btn btn-primary" type="submit" value="Submit" />
+            </div>
+         </form>
+          
+      </div>
+
     </div>
   );
 }
